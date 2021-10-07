18e8dc499cfa7a94e1ac3b41ba82526f495b1a4d
Administrator <admin@example.com>
2021-10-07 11:44:45 +0200

    3. Muestra resultados de busqueda

--- js/app.js
+++ js/app.js
@@ -42,7 +42,7 @@ function mostrarAlerta(mensaje) {
 
 function buscarImagenes(termino) {
     const key = '23741523-b9822c2077d62ea6a6d785841';
-    const url = `https://pixabay.com/api/?key=${key}&q=${termino}`;
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=100`;
 
     fetch(url)
         .then(respuesta => respuesta.json())
@@ -51,4 +51,33 @@ function buscarImagenes(termino) {
 
 function mostrarImagenes(imagenes) {
     console.log(imagenes);
+
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+
+    // Iterar sobre el arreglo de imagenes y construir el HTML
+    imagenes.forEach(imagen => {
+        const {previewURL, likes, views, largeImageURL} = imagen;
+
+        resultado.innerHTML +=`
+            <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+                <div class="bg-white">
+                    <img class="w-full" src="${previewURL}"></img>
+
+                    <div class="p-4">
+                        <p class="font-bold">${likes}<span class="font-light"> Me gusta</span></p>
+                        <p class="font-bold">${views}<span class="font-light"> veces vista</span></p>
+
+                        <a 
+                            class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1"
+                            href="${largeImageURL}" target="_blank" rel="noopener noreferrer"
+                        >
+                            Ver imagen
+                        </a>
+                    </div>
+                </div>
+            </div>
+            `
+    })
 }
